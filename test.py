aad5181432cf2cb0d2df157e8ff713ebd3982302
Sihao Chen <cs1819@ic.ac.uk>
2020-09-17 03:18:12 +0800

    change opt phase to test during test

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ opt.nThreads = 1  # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
-opt.phase = 'train'
+opt.phase = 'test'
 
 data_loader = CreateDataLoader(opt)
 dataset = data_loader.load_data()
