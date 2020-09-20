f760663b987fc0547a0d7c3ecb25df72c4717f11
Sihao Chen <cs1819@ic.ac.uk>
2020-09-20 20:24:37 +0800

    fix g_exp bug

--- models/networks.py
+++ models/networks.py
@@ -419,7 +419,7 @@ class ResnetGenerator(nn.Module):
 class ResnetGenerator_exposure(nn.Module):
     def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, gpu_ids=[], use_parallel = True, learn_residual = False, padding_type='reflect'):
         assert(n_blocks >= 0)
-        super(ResnetGenerator, self).__init__()
+        super(ResnetGenerator_exposure, self).__init__()
         self.input_nc = input_nc
         self.output_nc = output_nc
         self.ngf = ngf
