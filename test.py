bdcd67b4e245c544f8e71fc0a6711717f5225948
Sihao Chen <cs1819@ic.ac.uk>
2020-12-09 22:01:55 +0800

    change test code

--- test.py
+++ test.py
@@ -35,68 +35,80 @@ avgPSNR_b = 0.0
 #avgSSIM_b = 0.0
 counter = 0
 
-for i, data in enumerate(tqdm(dataset, total=opt.how_many)):
-    if i >= opt.how_many:
-        break
+exposure_list = [0.0000001, 0.01, 0.1, 0.2, 0.3]
+exposure_label = ['00', '01', '1', '2', '3']
+
+for i, data in enumerate(tqdm(dataset, total=len(dataset))):
+    #if i >= opt.how_many:
+    #    break
     counter = i
     model.set_input(data)
-    model.test(opt.exposure)
-    visuals = model.get_current_visuals()
-    visualizer.display_current_results(visuals, counter)
-    # PSNR_b = PSNR(visuals['syn_haze_img'], visuals['clear_img'])
-    # PSNR_d = PSNR(visuals['s_dehazing_img'], visuals['clear_img'])
-    # avgPSNR += PSNR_d
-    # avgPSNR_b += PSNR_b
-    #pilReala = Image.fromarray(visuals['real_A'])
-    #pilFake = Image.fromarray(visuals['fake_B'])
-    #pilReal = Image.fromarray(visuals['real_B'])
-    #SSIM_b = SSIM(pilReala,pilReal)
-    #SSIM_b = SSIM(pilReala).cw_ssim_value(pilReal)
-    #SSIM_d = SSIM(pilFake).cw_ssim_value(pilReal)
-    #avgSSIM += SSIM_d
-    #avgSSIM_b += SSIM_b
-    """DANET Evaluation
-    img_path = model.get_image_paths()
-    img = Image.fromarray(visuals['r_dehazing_img'], 'RGB')
-    img.save(os.path.join(opt.results_dir,'syn'+img_path[0].split('/')[-1]))
-    
-    img = Image.fromarray(visuals['real_haze_img'], 'RGB')
-    img.save(os.path.join(opt.results_dir,'ori_'+img_path[0].split('/')[-1]))
-    """
-    #CYCLEGAN Eval
-    if opt.model == 'cyclegan':
-        img_path = model.get_image_paths()
-        img = Image.fromarray(visuals['real_A'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_Syn'+'.png'))
+    for j, exposure in enumerate(exposure_list):
 
-        img = Image.fromarray(visuals['fake_B'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_S2R'+'.png'))
-        
-        img = Image.fromarray(visuals['rec_A'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_rec_Syn'+'.png'))
-        
-        img = Image.fromarray(visuals['real_B'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_Real'+'.png'))
-
-        img = Image.fromarray(visuals['fake_A'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_R2S'+'.png'))
-        
-        img = Image.fromarray(visuals['rec_B'], 'RGB')
-        img.save(os.path.join(opt.results_dir,str(counter)+'_rec_Real'+'.png'))
-
-    else:
+        model.test(exposure)
+        visuals = model.get_current_visuals()
+        visualizer.display_current_results(visuals, counter)
+        # PSNR_b = PSNR(visuals['syn_haze_img'], visuals['clear_img'])
+        # PSNR_d = PSNR(visuals['s_dehazing_img'], visuals['clear_img'])
+        # avgPSNR += PSNR_d
+        # avgPSNR_b += PSNR_b
+        #pilReala = Image.fromarray(visuals['real_A'])
+        #pilFake = Image.fromarray(visuals['fake_B'])
+        #pilReal = Image.fromarray(visuals['real_B'])
+        #SSIM_b = SSIM(pilReala,pilReal)
+        #SSIM_b = SSIM(pilReala).cw_ssim_value(pilReal)
+        #SSIM_d = SSIM(pilFake).cw_ssim_value(pilReal)
+        #avgSSIM += SSIM_d
+        #avgSSIM_b += SSIM_b
+        """DANET Evaluation
         img_path = model.get_image_paths()
         img = Image.fromarray(visuals['r_dehazing_img'], 'RGB')
         img.save(os.path.join(opt.results_dir,'syn'+img_path[0].split('/')[-1]))
         
         img = Image.fromarray(visuals['real_haze_img'], 'RGB')
         img.save(os.path.join(opt.results_dir,'ori_'+img_path[0].split('/')[-1]))
+        """
+        #CYCLEGAN Eval
+        if opt.model == 'cyclegan':
+            #img_path = model.get_image_paths()
 
-    # print('process image... %s ... Deblurred PSNR ... %f' % (img_path, PSNR_d))
-    # print('process image... %s ... Blurred PSNR ... %f ... Deblurred PSNR ... %f'
-    #     % (img_path, PSNR_b, PSNR_d))
-    #print('process image... %s ... Blurred SSIM ... %f ... Deblurred SSIM ... %f' % (img_path, SSIM_b, SSIM_d))
-    # visualizer.save_images(webpage, visuals, img_path)
+            if j == 0:
+                img = Image.fromarray(visuals['real_A'], 'RGB')
+                img.save(os.path.join(opt.results_dir,str(counter)+'_Input'+'.png'))
+
+                img = Image.fromarray(visuals['input_B'], 'RGB')
+                img.save(os.path.join(opt.results_dir,str(counter)+'_ExpertC'+'.png'))
+
+            img = Image.fromarray(visuals['fake_B'], 'RGB')
+            img.save(os.path.join(opt.results_dir,str(counter)+'_'+str(exposure_label[j])+'.png'))
+
+            """
+            img = Image.fromarray(visuals['rec_A'], 'RGB')
+            img.save(os.path.join(opt.results_dir,str(counter)+'_rec_Syn'+'.png'))
+            
+            img = Image.fromarray(visuals['real_B'], 'RGB')
+            img.save(os.path.join(opt.results_dir,str(counter)+'_Real'+'.png'))
+
+            img = Image.fromarray(visuals['fake_A'], 'RGB')
+            img.save(os.path.join(opt.results_dir,str(counter)+'_R2S'+'.png'))
+            
+            img = Image.fromarray(visuals['rec_B'], 'RGB')
+            img.save(os.path.join(opt.results_dir,str(counter)+'_rec_Real'+'.png'))
+            """
+
+        else:
+            img_path = model.get_image_paths()
+            img = Image.fromarray(visuals['r_dehazing_img'], 'RGB')
+            img.save(os.path.join(opt.results_dir,'syn'+img_path[0].split('/')[-1]))
+            
+            img = Image.fromarray(visuals['real_haze_img'], 'RGB')
+            img.save(os.path.join(opt.results_dir,'ori_'+img_path[0].split('/')[-1]))
+
+        # print('process image... %s ... Deblurred PSNR ... %f' % (img_path, PSNR_d))
+        # print('process image... %s ... Blurred PSNR ... %f ... Deblurred PSNR ... %f'
+        #     % (img_path, PSNR_b, PSNR_d))
+        #print('process image... %s ... Blurred SSIM ... %f ... Deblurred SSIM ... %f' % (img_path, SSIM_b, SSIM_d))
+        # visualizer.save_images(webpage, visuals, img_path)
 
 # avgPSNR /= counter
 # avgPSNR_b /= counter
